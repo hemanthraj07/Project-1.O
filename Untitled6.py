4bb0c2ba8bf30a10b8de1c8f2613834fd7683191
hemanthraj07 <89934721+hemanthraj07@users.noreply.github.com>
2021-09-01 23:07:34 +0530

    @collagefinder @cfdori @cfpython #collagefinder
    project 1.O @collagefinder @cfdori @cfpython #collagefinder

---
+++ Untitled6.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Project 1.0
+
+# In[1]:
+
+
+name = str(input("Name of the student: "))
+Class = str(input("which class you are studiying :  "))
+section = str(input("which section:  "))
+rollno = str(input("what is your Roll NO: "))
+maths = float(input("your percentage in maths: "))
+electronics = float(input("your percentage in electronics: "))
+CS= float(input("your percentage in Computerscience: "))
+
+
+# In[ ]:
+
+
+
+
